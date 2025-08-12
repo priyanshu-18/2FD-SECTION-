ac47ef946ae786af86fb0362380cb15749024eda
Priyanshu Pandey <122542895+priyanshu-18@users.noreply.github.com>
2025-08-12 09:43:24 +0530

    Add files via upload

---
+++ UserInput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter a no");
+        int a=sc.nextInt();
+        System.out.println(a);
+    }
+}

885cfb8275d533abf7a91e06408b14cdda0a3b6c
Impro Priyanshu <piyanshuphotos18@gmail.com>
2025-08-25 23:27:43 +0530

    learn the if else

---
+++ majduri/if_elsee.java
@@ -0,0 +1,14 @@
+package bal_majduri.java.majduri;
+
+public class if_elsee {
+    public static void main(String[] args){
+        int age =18;
+        if(age >=18){
+            System.out.println("Ok U Can Go To Goa");
+        }else{
+            System.out.println("go and watch pogo");
+        }
+    }
+
+}
+
